375deb16315fdab8857f45353ed0afbdfcbe79a2
Dmitry 'demqa' Stepanov <greatsteamguard@gmail.com>
2022-09-03 17:48:54 +0300

    First version of echo

---
+++ 1_echo.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    int  start_index =    1;
+    bool next_line   = true;
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) {
+        start_index++;
+        next_line = false;
+    }
+
+    for (int i = start_index; i < argc; i++) {
+        printf("%s", argv[i]);
+
+        if (i != argc - 1) {
+            putchar(' ');
+        }
+    }
+
+    if (next_line) {
+        printf("\n");
+    }
+
+    return 0;
+}
