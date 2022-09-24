0fe44448e84c0393240c246f4160df398cbc302c
Dmitry 'demqa' Stepanov <greatsteamguard@gmail.com>
2022-09-24 18:14:51 +0300

    4. My id can proceed one argument or zero. Yeaa-aye.

--- 4_id.cpp
+++ 4_id.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
@@ -18,15 +19,59 @@ void print_group(const group *group) {
 }
 
 void print_passwd(const passwd *passwd) {
-    assert(passwd!= nullptr);
+    assert(passwd != nullptr);
 
     printf("%d(%s)", passwd->pw_gid, passwd->pw_name);
 }
 
-int main(int argc, char **argv) {
-    assert(argc == 1);
+void print_groups(const gid_t *list, size_t number) {
+    printf("groups=");
+    for (size_t index = 0; index < number; ++index) {
+        group *group = getgrgid(list[index]);
+        print_group(group);
+        if (index + 1 != number) {
+            putchar(',');
+        }
+    }
 
+    putchar('\n');
+}
+
+int main(int argc, char **argv) {
     int status = 0;
+
+    if (argc > 2) {
+        fprintf(stderr, "myid: all arguments will be ignored except of first.\n");
+    }
+
+    if (argc == 2) {
+        passwd *passwd = getpwnam(argv[1]);
+        if (passwd == nullptr) {
+            fprintf(stderr, "myid: \'%s\': no such user.\n", argv[1]);
+            return EXIT_FAILURE;
+        }
+
+        printf("uid="); print_passwd(passwd); putchar(' ');
+
+        gid_t gid = passwd->pw_gid;
+        group *group_ = getgrgid(gid);
+        if (group_ == nullptr) {
+            fprintf(stderr, "myid: cannot get group by gid=%d, %s\n", gid, strerror(errno));
+        }
+        else {
+            printf("gid="); print_group(group_); putchar(' ');
+        }
+
+        int nelems = 0;
+        getgrouplist(argv[1], gid, nullptr, &nelems);
+
+        gid_t *groups = (gid_t *) calloc((size_t) nelems, sizeof(gid_t));
+        getgrouplist(argv[1], gid, groups, &nelems);
+
+        print_groups(groups, (size_t) nelems);
+
+        return EXIT_SUCCESS;
+    }
 
     uid_t euid = geteuid();
     printf("uid="); print_passwd(getpwuid(euid)); putchar(' ');
@@ -47,16 +92,7 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    printf("groups=");
-    for (size_t index = 0; index < status; ++index) {
-        group *current = getgrgid(list[index]);
-        print_group(current);
-        if (index + 1 != status) {
-            putchar(',');
-        }
-    }
-
-    putchar('\n');
+    print_groups(list, (size_t) status);
 
     free(list);
 
